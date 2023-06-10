5cdc7a0214d72f4114235efea78179915f0ec6d0
Sergio Benítez <sergio.kun21@gmail.com>
2023-06-10 12:43:22 -0500

    TS-016: handle expected result and error with ts-pattern.

--- projects/wikisearch/src/App.tsx
+++ projects/wikisearch/src/App.tsx
@@ -1,4 +1,4 @@
-import { Component, createEffect, createResource, For } from "solid-js";
+import { Component, createResource, For } from "solid-js";
 import * as E from 'fp-ts/lib/Either';
 import {match, P} from 'ts-pattern';
 import { z } from 'zod';
@@ -19,6 +19,37 @@ const App: Component = () => {
         <>
             <h1>Search Wikipedia</h1>
             <input onInput={(e) => setUserInput(e.currentTarget.value)} />
+            {match(searchResult())
+                .with({ _tag: 'Right'}, ({right: results}) => {
+                    <ul>
+                        <For each={results}>
+                            {(result) => (
+                                <li>
+                                    <a href={result.href} target="_blank">
+                                        {result.title}
+                                    </a>
+                                </li>
+                            )}
+                        </For>
+                    </ul>
+                })
+                .with({_tag: 'Left'}, ({left: err}) =>
+                    match(err)
+                        .with(P.instanceOf(ParserError), (err) => (
+                            <div>Error when parsing JSON!</div>
+                        ))
+                        .with(P.instanceOf(NetworkError), (err) => (
+                            <div>Error when connecting to server!</div>
+                        ))
+                        .with(P.instanceOf(z.ZodError), (err) => (
+                            <div>Unexpected data format from the server!</div>
+                        ))
+                        .otherwise((err) =>
+                            <div>And error ocurred fetching the wiki results, please try again</div>
+                        )
+                )
+
+            }
         </>
     );
 };
